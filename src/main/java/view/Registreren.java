2b3583a42f510c2d900fb9980278ff5de904b94c
JRichardson95 <71064586+JRichardson95@users.noreply.github.com>
2021-04-23 14:39:29 +0200

    Registreren refactored

--- src/main/java/view/Registreren.java
+++ src/main/java/view/Registreren.java
@@ -10,17 +10,15 @@ import model.entity.Gebruiker;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Registreren {
     private final EntityManager em =
             Persistence.createEntityManagerFactory("MySQL-Marktplaats").createEntityManager();
-
-
-    private BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-
+    View view = new View();
+    Gebruiker nieuweGebruiker = new Gebruiker();
+    private final BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
     private boolean adresVragen = false;
 
     public static void main(String[] args) {
@@ -33,25 +31,25 @@ public class Registreren {
 
     public void nieuweGebruiker() {
 
-        System.out.printf("Registreren nieuwe gebruiker\n");
+        view.printLine();
+        System.out.println("Registreren nieuwe gebruiker\n");
+        view.printLine();
 
-        Gebruiker nieuweGebruiker = new Gebruiker();
 
-        try {
-            System.out.println("Voornaam:");
-            nieuweGebruiker.setVoornaam(read.readLine());
-            System.out.println("Achternaam:");
-            nieuweGebruiker.setAchternaam(read.readLine());
-            System.out.println("Email:");
-            nieuweGebruiker.setEmail(read.readLine());
+        System.out.print("Voornaam:");
+        nieuweGebruiker.setVoornaam(readLine());
+        System.out.print("Achternaam:");
+        nieuweGebruiker.setAchternaam(readLine());
+        System.out.print("Email:");
+        nieuweGebruiker.setEmail(readLine());
+        view.printLine();
 
-            nieuweGebruiker.setBezorgwijzeSet(bezorgWijze());
-            if (adresVragen) nieuweGebruiker.setAdres(adresPrompt());
+        nieuweGebruiker.setBezorgwijzeSet(bezorgWijze());
 
-            nieuweGebruiker.setStatus(Status.ACTIEF);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        printBezorgwijze();
+
+        if (adresVragen) nieuweGebruiker.setAdres(adresPrompt());
+        nieuweGebruiker.setStatus(Status.ACTIEF);
 
         GebruikersDao gebruikersDao = new GebruikersDao(em);
         gebruikersDao.save(nieuweGebruiker);
@@ -62,53 +60,65 @@ public class Registreren {
     public Set<Bezorgwijze> bezorgWijze() {
         System.out.println("Welke bezorgwijze wilt u ondersteunen?");
         Arrays.asList(Bezorgwijze.values()).forEach(System.out::println);
+        return bezorgwijzeSet(readLine());
+    }
 
-        List<Integer> gekozenBezorgwijze = new ArrayList<Integer>();
-        String keuze = null;
-        try {
-            keuze = read.readLine();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        for (int i = 0; i < keuze.length(); i++) {
+    public Set<Bezorgwijze> bezorgwijzeSet(String keuzes) {
+        List<Integer> gekozenBezorgwijze = new ArrayList<>();
+
+        for (int i = 0; i < keuzes.length(); i++) {
+            if(Character.isDigit(keuzes.charAt(i))){
             gekozenBezorgwijze.add(Integer.parseInt(
-                    String.valueOf(keuze.charAt(i))) - 1);
+                    String.valueOf(keuzes.charAt(i))) - 1);
+            }
         }
 
-        if (gekozenBezorgwijze.contains(1)) adresVragen = true;
+        adresVragen = gekozenBezorgwijze.contains(1);
 
-
-        System.out.println("\nU heeft de volgende bezorgwijze geselecteerd:");
         Set<Bezorgwijze> bezorgwijzeSet = new HashSet<>();
         gekozenBezorgwijze.forEach(i -> bezorgwijzeSet.add(Bezorgwijze.values()[i]));
         return bezorgwijzeSet;
     }
 
+    private void printBezorgwijze() {
+        view.printLine();
+        System.out.println("U heeft de volgende bezorgwijzen geselecteerd:");
+        nieuweGebruiker.getBezorgwijzeSet().forEach(System.out::println);
+        view.printLine();
+    }
+
 
     public Adres adresPrompt() {
         Adres adres = new Adres();
-        try {
-            Postcode postcode = new Postcode();
-            System.out.println("-----------------------");
-            System.out.println("Wat is uw adres?");
-            System.out.println("Adres:");
-            System.out.println("    Straat:");
-            adres.setStraat(read.readLine());
-            System.out.println("    Huisnummer:");
-            adres.setHuisnummer(Integer.parseInt(read.readLine()));
-            System.out.println("    Postcode:");
-            String postcodeInput = read.readLine();
-            postcode.setPostcodeCijfers(Integer.parseInt(postcodeInput.substring(0, 4)));
-            postcode.setPostcodeLetters(postcodeInput.substring(4, 6));
-            postcode.setPostcode();
-            adres.setPostcode(postcode);
-            System.out.println("    Stad:");
-            adres.setStad(read.readLine());
-            System.out.println("    Provincie");
-            adres.setProvincie(read.readLine());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println("U heeft als bezorgwijze \"Thuis afhalen\" geselecteerd.");
+        System.out.println("Adres: ");
+        System.out.print("    Straat: ");
+        adres.setStraat(readLine());
+        System.out.print("    Huisnummer: ");
+        adres.setHuisnummer(Integer.parseInt(readLine()));
+        System.out.print("    Postcode: ");
+        adres.setPostcode(postcode(readLine()));
+        System.out.print("    Stad: ");
+        adres.setStad(readLine());
+        System.out.print("    Provincie: ");
+        adres.setProvincie(readLine());
         return adres;
+    }
+
+    public Postcode postcode(String postcodeInput) {
+        Postcode postcode = new Postcode();
+        postcode.setPostcodeCijfers(Integer.parseInt(postcodeInput.substring(0, 4)));
+        postcode.setPostcodeLetters(postcodeInput.substring(4, 6));
+        postcode.setPostcode();
+        return postcode;
+    }
+
+    public String readLine() {
+        try {
+            return read.readLine();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return "Er ging iets mis met het invoeren";
     }
 }
